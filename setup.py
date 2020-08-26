d28e3f20b1daad72a03924ea4cb326050db18434
kamo-naoyuki <naoyuki.kamo829@gmail.com>
2020-08-26 22:56:56 +0900

    fix  installation issue

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup, find_packages, Extension
-import numpy
+
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
 try:
     from Cython.Build import cythonize
     USE_CYTHON = True
@@ -9,12 +19,11 @@ except ImportError:
 # https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html
 ext = '.pyx' if USE_CYTHON else '.c'
 extensions = [
-    Extension("ctc_segmentation/ctc_segmentation_dyn",
-             ["ctc_segmentation/ctc_segmentation_dyn"+ext],
-             include_dirs=[numpy.get_include()])]
+    Extension("ctc-segmentation/ctc-segmentation-dyn",
+             ["ctc-segmentation/ctc-segmentation-dyn"+ext])]
 if USE_CYTHON:
     from Cython.Build import cythonize
-    extensions = cythonize(extensions, include_path=[numpy.get_include()])
+    extensions = cythonize(extensions)
 
 package_information = """
 # CTC segmentation
@@ -36,6 +45,7 @@ setup(
     # tests_require=["torch"],
     zip_safe=False,
     ext_modules=extensions,
+    cmdclass={'build_ext': build_ext},
 
     author="Ludwig Kuerzinger <ludwig.kuerzinger@tum.de>, "
            "Dominik Winkelbauer <dominik.winkelbauer@tum.de>",
